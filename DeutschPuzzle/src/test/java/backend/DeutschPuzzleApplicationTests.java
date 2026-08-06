a9056445d5c2dcb8b728470716fe0f83748e8f71
Your Name <you@example.com>
2026-08-06 15:24:24 +0200

    adding assertion for CI test

--- DeutschPuzzle/src/test/java/backend/DeutschPuzzleApplicationTests.java
+++ DeutschPuzzle/src/test/java/backend/DeutschPuzzleApplicationTests.java
@@ -2,12 +2,14 @@ package backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DeutschPuzzleApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1, 3);
     }
 
 }
