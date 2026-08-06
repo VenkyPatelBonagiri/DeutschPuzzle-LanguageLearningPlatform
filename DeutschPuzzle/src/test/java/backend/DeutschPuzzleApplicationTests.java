ec01b58fe0f8854fde53c9fa0cf34323877f313b
Your Name <you@example.com>
2026-08-06 15:22:01 +0200

    removing assert condition

--- DeutschPuzzle/src/test/java/backend/DeutschPuzzleApplicationTests.java
+++ DeutschPuzzle/src/test/java/backend/DeutschPuzzleApplicationTests.java
@@ -2,14 +2,12 @@ package backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DeutschPuzzleApplicationTests {
 
     @Test
     void contextLoads() {
-        assertEquals(1, 3);
     }
 
 }
